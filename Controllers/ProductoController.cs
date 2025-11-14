b0b8636b660ff74a720eb990143a6a2d99acc781
DiazRobertoNicolas <nicolas0405.nd@gmail.com>
2025-11-14 03:03:22 +0000

    TP8 Terminado

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using Microsoft.AspNetCore.Mvc;
 
 namespace tl2_tp8_2025_nicoodiaz;
@@ -23,4 +24,43 @@ public class ProductoController : Controller
         Producto producto = _productoRepository.ObtenerProductoXId(id);
         return producto != null ? View(producto) : NotFound();
     }
-}
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Producto nuevoProducto)
+    {
+        _productoRepository.CrearProducto(nuevoProducto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        return View(_productoRepository.ObtenerProductoXId(id));
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Producto prodAElim)
+    {
+        _productoRepository.EliminarProducto(prodAElim.IdProducto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var producto = _productoRepository.ObtenerProductoXId(id);
+        return View(producto);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Producto prod)
+    {
+        _productoRepository.ActualizarProducto(prod.IdProducto, prod);
+        return RedirectToAction("Index");
+    }
+} 
